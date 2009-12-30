f8974727fab95251cd2cc7bda73dad78d3fdcc4e
Thomas Giesel <skoe@directbox.com>
2009-12-30 21:40:13 +0100

    EF2: GeoRAM and EasyFlash work

--- Hardware/ef2-vhdl/src/ef2.vhdl
+++ Hardware/ef2-vhdl/src/ef2.vhdl
@@ -4,7 +4,7 @@
 --
 -- Registers used:
 -- 
--- 19 mem_addr (will be 21 when flash memory is being used)
+-- 21 mem_addr (will be 21 when flash memory is being used)
 --  8 mem_data
 --  1 mem_data tristate enable
 --  1 n_mem_wr
@@ -13,13 +13,14 @@
 --  1 n_reset tristate enable
 --  1 n_led
 -- ==
--- 33
+-- 35
 -- 
 -- 11 ram_bank
+--  7 flash_bank
 --  1 buttons_enabled
---  2 cart_mode
+--  1 cart_mode
 -- ==
--- 14
+-- 20
 -- 
 -- component exp_bus_ctrl (u0):
 --  2 FDCPE_u0/bus_current_state_i
@@ -46,8 +47,8 @@ entity ef2 is
            n_io1:       in std_logic;
            n_io2:       in std_logic;
            n_wr:        in std_logic;
-           n_irq:       out std_logic;
-           n_nmi:       out std_logic;
+           n_irq:       inout std_logic;
+           n_nmi:       inout std_logic;
            n_reset:     inout std_logic;
            n_dotclk:    in std_logic;
            phi2:        in std_logic;
@@ -81,20 +82,28 @@ architecture ef2_arc of ef2 is
     signal bus_current_state: bus_state_type;
 
     -- output enable for expansion port data bus
-    signal bus_out_enable:  std_logic;
+    signal bus_out_enable: std_logic;
 
     -- Memory bank for RAM. In GeoRAM and EasyFlash mode 256 bytes of RAM
     -- are visible at once. So this bank will go to memory bits 18 downto 8
     -- for 512 KiB.
     signal ram_bank: std_logic_vector(10 downto 0);
 
+    -- Memory bank for Flash ROM. Usually 8 KiB of ROM can be visible at
+    -- LOROM and 8 KiB of ROM can be seen at HIROM. So this bank will go to 
+    -- memory bits 19 downto 13 for 1 MiB (bit 20 is for HIROM/LOROM).
+    signal flash_bank: std_logic_vector(6 downto 0);
+
     signal buttons_enabled: std_logic := '0';
 
     -- Current cartridge mode
-    type cartridge_mode is (MODE_GEORAM, MODE_WARPSPEED64, MODE_WARPSPEED128);
+    type cartridge_mode is (MODE_GEORAM, MODE_EASYFLASH);
 
     signal cart_mode: cartridge_mode := MODE_GEORAM;
 
+    -- When we are in easyflash mode: boot enabled?
+    signal easyflash_boot: std_logic := '1';
+    
     component exp_bus_ctrl is
         port 
         (  
@@ -129,8 +138,7 @@ begin
     n_irq <= 'Z';
     n_nmi <= 'Z';
     n_reset <= 'Z';
-    n_exrom <= 'Z';
-    n_game <= 'Z';
+    n_mem_reset <= '1'; 
     pad2 <= '1';
     pad3 <= '1';
     pad4 <= '1';
@@ -155,7 +163,7 @@ begin
     -- Check the cartridge buttons. If one is pressed, reset the C64 and 
     -- activate the cartridge mode according to the buttons
     ---------------------------------------------------------------------------
-    switch_cartmode: process(buttons_enabled, button_a, button_b, button_c)
+    switch_cartmode: process(n_dotclk)
     begin
         if rising_edge(n_dotclk) then
             n_reset <= 'Z';
@@ -163,12 +171,21 @@ begin
                 if button_a = '1' then
                     cart_mode <= MODE_GEORAM;
                     n_reset <= '0';
+
                 elsif button_b = '1' then
-                    cart_mode <= MODE_WARPSPEED64;
+                    cart_mode <= MODE_EASYFLASH;
+                    easyflash_boot <= '1';
                     n_reset <= '0';
-                elsif button_c = '1' then
-                    cart_mode <= MODE_WARPSPEED128;
-                    n_reset <= '0';
+
+                elsif button_d = '1' then
+                    -- This button has a special function depending from mode
+                    case cart_mode is
+                        when MODE_EASYFLASH =>
+                            easyflash_boot <= '0';
+                            n_reset <= '0';
+
+                        when others => null;
+                    end case;
                 end if;
             end if;
         end if;
@@ -177,7 +194,7 @@ begin
     ---------------------------------------------------------------------------
     -- Set the state of the LED.
     ---------------------------------------------------------------------------
-    set_led: process(n_dotclk, button_a, button_b, button_c, button_d)
+    set_led: process(n_dotclk)
     begin
         if rising_edge(n_dotclk) then
             n_led <= '1';
@@ -205,6 +222,37 @@ begin
     end process data_to_port;
 
     ---------------------------------------------------------------------------
+    --
+    ---------------------------------------------------------------------------
+    set_game_exrom: process(n_dotclk, n_reset)
+    begin
+        if rising_edge(n_dotclk) then
+            case cart_mode is
+                when MODE_GEORAM =>
+                    n_exrom <= '1';
+                    n_game <= '1';
+                    
+                when MODE_EASYFLASH =>
+                    if n_reset = '0' then
+                        n_exrom <= '1';
+                        n_game <= not easyflash_boot;
+                    elsif bus_next_state = BUS_WRITE_ENABLE and 
+                          n_io1 = '0' and addr(1) = '1' then
+                        -- $de02 (only addr(1) is checked in the original EF)
+                        n_exrom <= not data(1);
+                        if data(2) = '0' then
+                            n_game <= not easyflash_boot;
+                        else
+                            n_game <= not data(0);
+                        end if;
+                    end if;
+
+                when others => null;
+            end case;
+        end if;      
+    end process set_game_exrom;
+
+    ---------------------------------------------------------------------------
     -- Put the addresses onto the memory address bus
     ---------------------------------------------------------------------------
     prepare_mem_address: process(n_dotclk)
@@ -212,28 +260,48 @@ begin
         if rising_edge(n_dotclk) then
             if bus_next_state = BUS_READ_VALID or 
                bus_next_state = BUS_WRITE_VALID then
-                -- GeoRAM: Show current RAM bank at $dexx
-                if n_io1 = '0' then
-                    mem_addr(20 downto 8) <= "00" & ram_bank;
-                end if;
+
+                case cart_mode is
+                    when MODE_GEORAM =>
+                        -- Show current RAM bank at $dexx
+                        if n_io1 = '0' then
+                            mem_addr(20 downto 8) <= "00" & ram_bank;
+                        end if;
+
+                    when MODE_EASYFLASH =>
+                        if n_io2 = '0' then
+                            -- Show RAM bank 0 at $dfxx                            
+                            mem_addr(20 downto 8) <= (others => '0');
+                        elsif n_roml = '0' then
+                            -- Show current Flash bank at ROML or ROMH
+                            mem_addr(20 downto 8) <= 
+                                "0" & flash_bank & addr(12 downto 8);
+                        elsif n_romh = '0' then
+                            mem_addr(20 downto 8) <= 
+                                "1" & flash_bank & addr(12 downto 8);
+                        end if;
+
+                    when others => null;
+                end case;
+                
                 mem_addr(7 downto 0) <= addr(7 downto 0);
             end if;
         end if;
     end process prepare_mem_address;
 
     ---------------------------------------------------------------------------
-    -- Put the addresses onto the memory address bus
+    -- Copy expansion bus data to memory bus data
     ---------------------------------------------------------------------------
     prepare_mem_data: process(n_dotclk)
     begin
         if rising_edge(n_dotclk) then
+
             mem_data <= (others => 'Z');
+
+            -- Is this correct ???
             if bus_next_state = BUS_WRITE_VALID or
                bus_next_state = BUS_WRITE_ENABLE then
-                -- GeoRAM: Show current RAM bank at $dexx
-                if n_io1 = '0' then
-                    mem_data <= data;
-                end if;
+                mem_data <= data;
             end if;
         end if;
     end process prepare_mem_data;
@@ -243,8 +311,6 @@ begin
     ---------------------------------------------------------------------------
     mem_control: process(n_dotclk, n_reset)
     begin
-        n_mem_reset <= n_reset;
-
         if rising_edge(n_dotclk) then
 
             case bus_next_state is
@@ -256,18 +322,50 @@ begin
                     n_mem_oe    <= '1';
 
                 when BUS_READ_VALID =>
-                    -- GeoRAM: Read RAM at $de00
-                    if n_io1 = '0' then
-                        n_ram_cs   <= '0';
-                        n_mem_oe   <= '0';
-                    end if;
+                    case cart_mode is
+                        when MODE_GEORAM =>
+                            if n_io1 = '0' then
+                                -- Read RAM at $de00
+                                n_ram_cs   <= '0';
+                                n_mem_oe   <= '0';
+                            end if;
+
+                        when MODE_EASYFLASH =>
+                            if n_io2 = '0' then
+                                -- Read RAM at $df00
+                                n_ram_cs   <= '0';
+                                n_mem_oe   <= '0';
+                            elsif n_roml = '0' or n_romh = '0' then
+                                -- Read FLASH at ROML/ROMH
+                                n_flash_cs <= '0';
+                                n_mem_oe   <= '0';
+                            end if;
+
+                        when others => null;
+                    end case;
 
                 when BUS_WRITE_ENABLE =>
-                    -- GeoRAM: Write RAM at $de00
-                    if n_io1 = '0' then
-                        n_ram_cs   <= '0';
-                        n_mem_wr   <= '0';
-                    end if;
+                    case cart_mode is
+                        when MODE_GEORAM =>
+                            if n_io1 = '0' then
+                                -- Write RAM at $de00
+                                n_ram_cs   <= '0';
+                                n_mem_wr   <= '0';
+                            end if;
+
+                        when MODE_EASYFLASH =>
+                            if n_io2 = '0' then
+                                -- Write RAM at $df00
+                                n_ram_cs   <= '0';
+                                n_mem_wr   <= '0';
+                            elsif n_roml = '0' or n_romh = '0' then
+                                -- Write FLASH at ROML/ROMH
+                                n_flash_cs <= '0';
+                                n_mem_wr   <= '0';
+                            end if;
+
+                        when others => null;
+                    end case;
 
                 when others => null;
             end case;
@@ -282,27 +380,56 @@ begin
     --     we check the lowest bit of the address only
     --     todo: how many bits did the original GeoRAM check?
     -- In EasyFlash mode:
-    --     RAM bank always 0
-    --     todo: Would it be useful to use the last page of RAM?
+    --     RAM bank not used, always 0
     ---------------------------------------------------------------------------
     set_ram_bank: process(n_dotclk, n_reset)
     begin
         if n_reset = '0' then
             ram_bank <= (others => '0');
         elsif rising_edge(n_dotclk) then
-            -- GeoRAM
-            if bus_next_state = BUS_WRITE_ENABLE and n_io2 = '0' then
-                -- todo: Wie sieht das Register-Mirroring bei der 
-                --       Original-GeoRAM aus?
-                -- $dffe
-                if addr(0) = '0' then
-                    ram_bank(5 downto 0)  <= data(5 downto 0);
-                -- $dfff
-                else
-                    ram_bank(10 downto 6) <= data(4 downto 0);
-                end if;
+            if bus_next_state = BUS_WRITE_ENABLE then
+                case cart_mode is
+                    when MODE_GEORAM =>
+                        if n_io2 = '0' then
+                            -- todo: Wie sieht das Register-Mirroring bei der 
+                            --       Original-GeoRAM aus?
+                            -- $dffe
+                            if addr(0) = '0' then
+                                ram_bank(5 downto 0)  <= data(5 downto 0);
+                            -- $dfff
+                            else
+                                ram_bank(10 downto 6) <= data(4 downto 0);
+                            end if;
+                        end if;
+                
+                    when others => null;
+                end case;
             end if;
         end if;
     end process set_ram_bank;
+
+    ---------------------------------------------------------------------------
+    -- Set the Flash ROM bank.
+    -- In EasyFlash mode:
+    --     $de00 select flash bank 8 KiB block (i.e. bits 19 downto 13)
+    ---------------------------------------------------------------------------
+    set_flash_bank: process(n_dotclk, n_reset)
+    begin
+        if n_reset = '0' then
+            flash_bank <= (others => '0');
+        elsif rising_edge(n_dotclk) then
+            if bus_next_state = BUS_WRITE_ENABLE then
+                case cart_mode is
+                    when MODE_EASYFLASH =>
+                        -- $de00 (only addr(1) is checked in the original EF)
+                        if n_io1 = '0' and addr(1) = '0' then
+                            flash_bank(6 downto 0) <= data(6 downto 0);
+                        end if;
+                    
+                    when others => null;
+                end case;
+            end if;
+        end if;
+    end process set_flash_bank;
 
 end ef2_arc;
