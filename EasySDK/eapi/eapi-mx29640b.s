12dc94696c48e08f8b5c3e63594b659f8f5ed800
tgiesel <tgiesel@localhost>
2011-06-25 00:46:21 +0200

    EAPI for MX29LV640EB added

--- EasySDK/eapi/eapi-mx29640b.s
+++ EasySDK/eapi/eapi-mx29640b.s
@@ -27,9 +27,9 @@ FLASH_ALG_ERROR_BIT      = $20
 EAPI_ZP_INIT_CODE_BASE   = $4b
 
 ; hardware dependend values
-M29W160ET_NUM_BANKS     = 128
-M29W160ET_MFR_ID        = $20
-M29W160ET_DEV_ID        = $c4
+MX29LV640EB_NUM_BANKS    = 64
+MX29LV640EB_MFR_ID       = $c2
+MX29LV640EB_DEV_ID       = $cb
 
 EAPI_RAM_CODE           = $df80
 EAPI_RAM_SIZE           = 124
@@ -41,8 +41,8 @@ EAPI_RAM_SIZE           = 124
 EAPICodeBase:
         !byte $65, $61, $70, $69        ; signature "EAPI"
 
-        !pet "M29F160ET V1.0"
-        !byte 0, 0                      ; 16 bytes, must be 0-terminated
+        !pet "MX29LV640EB 1.0"
+        !byte 0                         ; 16 bytes, must be 0-terminated
 
 ; =============================================================================
 ;
@@ -270,10 +270,10 @@ ciNoRamError:
         lda $8002
         sta EAPI_TMP_VAL2
 
-        ; check if it is an M29W160ET
-        cpx #M29W160ET_MFR_ID
+        ; check if it is an MX29LV640EB
+        cpx #MX29LV640EB_MFR_ID
         bne ciNotSupported
-        cmp #M29W160ET_DEV_ID
+        cmp #MX29LV640EB_DEV_ID
         bne ciNotSupported
 
         ; everything okay
@@ -296,7 +296,7 @@ returnOnly:                     ; C indicates error
         lda EAPI_TMP_VAL2       ; device or error code in A
         bcs returnCSet
         ldx EAPI_TMP_VAL1       ; manufacturer in X
-        ldy #M29W160ET_NUM_BANKS ; number of banks in Y
+        ldy #MX29LV640EB_NUM_BANKS ; number of banks in Y
 
         plp
         clc                     ; do this after plp :)
