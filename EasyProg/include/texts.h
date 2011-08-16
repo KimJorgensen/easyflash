9ae179813585d3c6c9c1ad905756899b75382d88
skoe <skoe@localhost>
2011-08-16 23:15:56 +0200

    Slot selection fixed

--- EasyProg/include/texts.h
+++ EasyProg/include/texts.h
@@ -21,6 +21,7 @@ extern const char* apStrBadRAM[];
 extern const char* apStrROMLProtected[];
 extern const char* apStrROMHProtected[];
 extern const char* apStrAskErase[];
+extern const char* apStrSlot0[];
 extern const char* apStrEraseFailed[];
 extern const char* apStrFlashWriteFailed[];
 extern const char* apStrFileOpenError[];
