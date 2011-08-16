46845d2095d084f94d9af1b290092c3504d3c694
skoe <skoe@localhost>
2011-08-16 23:15:56 +0200

    Slot selection fixed

--- EasyProg/include/slots.h
+++ EasyProg/include/slots.h
@@ -4,6 +4,11 @@
 
 #include <stdint.h>
 
-uint8_t __fastcall__ selectSlot(uint8_t nSlots);
+uint8_t __fastcall__ selectSlotDialog(uint8_t nSlots);
+void __fastcall__ checkAskForSlot(uint8_t bWarn);
+void selectSlot0(void);
+
+extern uint8_t g_nSelectedSlot;
+extern uint8_t g_nSlots;
 
 #endif
