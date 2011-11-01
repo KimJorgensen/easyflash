54372944ce69a10f168b47bcf21162da98bcad54
skoe <skoe@localhost>
2011-11-01 22:37:52 +0100

    simplified design on the cost of macrocells, EF flash also in I/O area

--- Hardware/ef3-vhdl/src/cart_kernal.vhdl
+++ Hardware/ef3-vhdl/src/cart_kernal.vhdl
@@ -26,27 +26,24 @@ use ieee.numeric_std.all;
 
 entity cart_kernal is
     port (
-        clk:        in  std_logic;
-        n_reset:    in  std_logic;
-        enable:     in  std_logic;
-        phi2:       in  std_logic;
-        ba:         in  std_logic;
-        n_romh:     in  std_logic;
-        n_wr:       in  std_logic;
-        addr_ready: in  std_logic;
-        bus_ready:  in  std_logic;
-        hiram_detect_ready: in std_logic;
-        cycle_start: in  std_logic;
-        addr:       in  std_logic_vector(15 downto 0);
-        mem_addr:   out std_logic_vector(12 downto 0);
-        latch_mem_addr: out std_logic;
-        ma19:       out std_logic;
-        latch_ma19: out std_logic;
-        a14:        out std_logic;
-        n_game:     out std_logic;
-        n_exrom:    out std_logic;
-        flash_read: out std_logic;
-        hiram:      out std_logic
+        clk:                in  std_logic;
+        n_reset:            in  std_logic;
+        enable:             in  std_logic;
+        phi2:               in  std_logic;
+        ba:                 in  std_logic;
+        n_romh:             in  std_logic;
+        n_wr:               in  std_logic;
+        addr_ready:         in  std_logic;
+        bus_ready:          in  std_logic;
+        hiram_detect_ready: in  std_logic;
+        cycle_start:        in  std_logic;
+        addr:               in  std_logic_vector(15 downto 0);
+        flash_addr:         out std_logic_vector(22 downto 0);
+        a14:                out std_logic;
+        n_game:             out std_logic;
+        n_exrom:            out std_logic;
+        flash_read:         out std_logic;
+        hiram:              out std_logic
     );
 end cart_kernal;
 
@@ -102,31 +99,33 @@ begin
     end process;
 
     ---------------------------------------------------------------------------
+    -- Combinatorically create the next memory address.
+    -- Always read from part usually used for ROML, because there we have
+    -- the boot sectors which contain the KERNAL images
+    ---------------------------------------------------------------------------
+    create_mem_addr: process(addr)
+    begin
+        flash_addr <= (others => '0');
+        -- todo: KERNAL-Nummer
+        flash_addr(12 downto 0) <= addr(12 downto 0);
+    end process;
+
+    ---------------------------------------------------------------------------
     -- Combinatorical logic to prepare the address to be latched to the
     -- internal address bus. Additionally the signal which enables
     -- the latches at the next CLK cycle is prepared here.
     ---------------------------------------------------------------------------
-    create_mem_addr: process(enable, addr, addr_ready,
+    create_mem_ctrl: process(enable, addr, addr_ready,
                              kernal_space_cpu_read)
     begin
         flash_read <= '0';
-        mem_addr <= (others => '0');
-        latch_mem_addr <= '0';
-        latch_ma19 <= '0';
 
         if enable = '1' then
             if addr_ready = '1' and kernal_space_cpu_read then
-                    -- start speculative flash read to hide its latency
-                    mem_addr <= addr(12 downto 0);
-                    latch_mem_addr <= '1';
-                    latch_ma19 <= '1';
-                    flash_read <= '1';
+                -- start speculative flash read to hide its latency
+                flash_read <= '1';
             end if;
         end if;
     end process;
-
-    -- always read from part usually used for ROML, because there we have
-    -- the boot sectors which contain the KERNAL images
-    ma19 <= '0';
 
 end architecture behav;
