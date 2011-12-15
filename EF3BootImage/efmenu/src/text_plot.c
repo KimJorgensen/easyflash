825b66f700118289b3e029142b6948283c44db58
skoe <skoe@localhost>
2011-12-15 23:10:15 +0100

    Text plotter optimized

--- EF3BootImage/efmenu/src/text_plot.c
+++ EF3BootImage/efmenu/src/text_plot.c
@@ -22,15 +22,16 @@
  * Thomas Giesel skoe@directbox.com
  */
 
- 
+
 #include <stdint.h>
 #include <string.h>
 #include <c64.h>
 #include "memcfg.h"
 #include "text_plot.h"
 
+/* These are also used by text_plot_asm */
 uint16_t text_plot_x;
-uint8_t  text_plot_y;
+uint8_t* text_plot_addr;
 
 
 /******************************************************************************/
@@ -38,20 +39,27 @@ uint8_t  text_plot_y;
  * Plot the given 0-terminated string into the bitmap at x_pos/y_pos.
  *
  * No clipping is performed.
- * x_pos/y_pos are the upper left-hand corner of the first character.
+ * x_pos/y_pos are the upper left-hand corner of the first character in block
+ * coordinates. x_offset is an additional pixel offset 0..7 which is added to
+ * the X-Position.
  */
 void __fastcall__ text_plot_puts(
-        uint16_t x_pos, uint8_t y_pos, const char* str)
+        uint8_t x_pos, uint8_t x_offset, uint8_t y_pos,
+        uint8_t color, const char* str)
 {
-    text_plot_x = x_pos;
-    text_plot_y = y_pos;
-
-    while (*str != 0)
-    {
-        text_plot_char(*str);
-        
-        // space between chars
-        ++text_plot_x;
-        ++str;
-    }
+    text_plot_x = 8 * x_pos + x_offset;
+    text_plot_addr = P_GFX_BITMAP + y_pos * 320 + x_pos * 8;
+    text_plot_str(str);
 }
+
+/******************************************************************************/
+/**
+ * Set the color of 16 blocks at position x/y.
+ */
+void __fastcall__ text_set_line_color(
+        uint8_t x_pos, uint8_t y_pos, uint8_t color)
+{
+    text_plot_addr = P_GFX_COLOR + y_pos * 40 + x_pos;
+    text_fill_line_color((16 << 8) | color);
+}
+
