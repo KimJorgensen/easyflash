b1524aaf74b674446deebb31fb0ebe2246ad4bb8
skoe <skoe@localhost>
2012-02-03 00:10:23 +0100

    ef3usb_send_data made public

--- libs/libef3usb/src/ef3usb.h
+++ libs/libef3usb/src/ef3usb.h
@@ -8,11 +8,14 @@
 #ifndef USB_H_
 #define USB_H_
 
+#include <stdint.h>
+
 char*             ef3usb_check_cmd(void);
+void __fastcall__ ef3usb_send_data(const uint8_t* data, uint16_t len);
 void __fastcall__ ef3usb_send_str(const char* p);
 void              ef3usb_discard_rx(void);
 
-/* these functions can be used after usbSendResponseLOAD(): */
+/* these functions can be used after ef3usb_send_str("load"): */
 unsigned int __fastcall__ ef3usb_fread(void* buffer, unsigned int size);
 void*                     ef3usb_fload(void);
 void                      ef3usb_fclose(void);
