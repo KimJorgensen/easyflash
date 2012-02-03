8480c7ee4c463ec75dd1aed351fb9c9a410a0483
skoe <skoe@localhost>
2012-02-03 23:11:01 +0100

    function to read raw data from USB

--- libs/libef3usb/src/ef3usb.h
+++ libs/libef3usb/src/ef3usb.h
@@ -10,14 +10,19 @@
 
 #include <stdint.h>
 
-char*             ef3usb_check_cmd(void);
+char* ef3usb_check_cmd(void);
+
+uint16_t __fastcall__ ef3usb_receive_data(void* buffer, uint16_t size);
+
 void __fastcall__ ef3usb_send_data(const uint8_t* data, uint16_t len);
+
 void __fastcall__ ef3usb_send_str(const char* p);
-void              ef3usb_discard_rx(void);
+
+void ef3usb_discard_rx(void);
 
 /* these functions can be used after ef3usb_send_str("load"): */
-unsigned int __fastcall__ ef3usb_fread(void* buffer, unsigned int size);
-void*                     ef3usb_fload(void);
-void                      ef3usb_fclose(void);
+uint16_t __fastcall__ ef3usb_fread(void* buffer, uint16_t size);
+void* ef3usb_fload(void);
+void ef3usb_fclose(void);
 
 #endif /* USB_H_ */
--- libs/libef3usb/src/ef3usb_fread.s
+++ libs/libef3usb/src/ef3usb_fread.s
@@ -14,22 +14,46 @@ m_size_hi    = tmp1
 
 ; =============================================================================
 ;
-; unsigned int __fastcall__ ef3usb_fread(void* buffer, unsigned int size);
+; uint16_t __fastcall__ ef3usb_receive_data(void* buffer, uint16_t size);
 ;
-; Reads up to "size" bytes from USB to "buffer".
-; Returns the number of bytes actually read, 0 if there are no bytes left
-; (EOF).
+; Reads exactly size bytes from USB to buffer. Do not return before the
+; all data has been received. Return size.
+;
+; =============================================================================
+.proc   _ef3usb_receive_data
+.export _ef3usb_receive_data
+_ef3usb_receive_data:
+        jsr get_args
+
+        lda xfer_size_zp
+        ldy xfer_size_zp + 1
+        jmp ef3usb_read_common
+.endproc
+
+get_args:
+        sta size_zp
+        sta xfer_size_zp
+        stx size_zp + 1         ; Save size
+        stx xfer_size_zp + 1
+
+        jsr popax
+        sta p_buff_zp
+        stx p_buff_zp + 1       ; Save buffer address
+        rts
+
+; =============================================================================
+;
+; uint16_t __fastcall__ ef3usb_fread(void* buffer, uint16_t size);
+;
+; Request size bytes from the host. Read up to size bytes from USB to
+; buffer. Returns the number of bytes actually read, 0 if there are no bytes
+; left (EOF).
 ;
 ; =============================================================================
 .proc   _ef3usb_fread
 .export _ef3usb_fread
 _ef3usb_fread:
-        sta size_zp
-        stx size_zp + 1         ; Save size
-
-        jsr popax
-        sta p_buff_zp
-        stx p_buff_zp + 1       ; Save buffer address
+        jsr get_args
 
         ldx size_zp
         ldy size_zp + 1
@@ -46,12 +70,14 @@ _ef3usb_fread:
         wait_usb_rx_ok
         ldy USB_DATA            ; high byte of transfer size
         sty xfer_size_zp + 1
-
-        bne @loadCont
+        bne ef3usb_read_common
         cpx #0                  ; check for EOF
-        beq @end                ; 0 bytes == EOF
-@loadCont:
+        beq end                 ; 0 bytes == EOF
         txa
+.endproc
+
+ef3usb_read_common:
+        ; here: A = xfer_size low, Y = xfer_size high
         eor #$ff
         tax
         tya
@@ -72,8 +98,7 @@ _ef3usb_fread:
         bne @getBytes
         inc m_size_hi
         bne @getBytes
-@end:
+end:
         lda xfer_size_zp
         ldx xfer_size_zp + 1            ; return number of bytes transfered
         rts
-.endproc
