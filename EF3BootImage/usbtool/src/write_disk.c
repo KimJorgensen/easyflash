8972d0f68fc236c0ae42fd2338ae7d40038c95b8
skoe <skoe@localhost>
2012-02-03 23:08:08 +0100

    First disk written

--- EF3BootImage/usbtool/src/write_disk.c
+++ EF3BootImage/usbtool/src/write_disk.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <c64.h>
 
 #include <ef3usb.h>
 #include <eload.h>
@@ -9,7 +10,19 @@
 #include "usbtool.h"
 
 
-#define WRITE_DISK_ERR_WRONG_DRIVE      80
+#define DISK_STATUS_MAGIC            0x52
+
+/* These error codes are the same as the ones for 1541 job codes */
+#define DISK_STATUS_OK               0x01 /* Everything OK */
+#define DISK_STATUS_HEADER_NOT_FOUND 0x02 /* Header block not found */
+#define DISK_STATUS_SYNC_NOT_FOUND   0x03 /* SYNC not found */
+#define DISK_STATUS_DATA_NOT_FOUND   0x04 /* Data block not found */
+#define DISK_STATUS_DATA_CHK_ERR     0x05 /* Checksum error in data block */
+#define DISK_STATUS_VERIFY_ERR       0x07 /* Verify error */
+#define DISK_STATUS_WRITE_PROTECTED  0x08 /* Disk write protected */
+#define DISK_STATUS_HEADER_CHK_ERR   0x09 /* Checksum error in header block */
+#define DISK_STATUS_ID_MISMATCH      0x0b /* Id mismatch */
+#define DISK_STATUS_NO_DISK          0x0f /* Disk not inserted */
 
 
 /******************************************************************************/
@@ -47,7 +60,10 @@ static uint8_t init_eload(uint8_t drv)
  */
 void write_disk_d64(void)
 {
-    static uint8_t a_request[2];
+    static uint8_t a_status[2];
+    static uint8_t a_ts[2 + 22];
+    static uint8_t a_track_data[21][256];
+    uint8_t i, n_track, n_sector;
 
     puts("\nd64 writer started");
     ef3usb_send_str("load");
@@ -59,12 +75,37 @@ void write_disk_d64(void)
         return;
     }
 
-    puts("Preparing drive...");
     eload_prepare_drive();
-    puts("Drive ok");
+    puts("Drive ready");
 
-    puts("<= CLOSE");
-    a_request[0] = 0;
-    a_request[1] = 0;
-    ef3usb_send_data(a_request, 2);
+    // disable VIC-II DMA
+    VIC.ctrl1 &= 0xef;
+    while (VIC.rasterline != 255)
+    {}
+
+    for (;;)
+    {
+        a_status[0] = DISK_STATUS_MAGIC;
+        a_status[1] = DISK_STATUS_OK;
+        ef3usb_send_data(a_status, 2);
+
+        ef3usb_receive_data(a_ts, sizeof(a_ts));
+        if (a_ts[1] == 0)
+            break;
+
+        /* load the number of sectors for this track */
+        ef3usb_receive_data(a_track_data, a_ts[1] * 256);
+
+        n_track = a_ts[0];
+        i = 2;
+        while ((n_sector = a_ts[i]) != 0xff)
+        {
+            eload_write_sector_nodma((n_track << 8) | n_sector,
+                                     a_track_data[n_sector]);
+            ++i;
+        }
+    }
+
+    // enable VIC-II DMA
+    VIC.ctrl1 |= 0x10;
 }
