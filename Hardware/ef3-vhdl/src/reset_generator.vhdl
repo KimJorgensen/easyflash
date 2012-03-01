4a7860dc7348e30bc15f1d4110bd5b30750ce942
skoe <skoe@localhost>
2012-03-01 21:45:31 +0100

    Comments improved

--- Hardware/ef3-vhdl/src/reset_generator.vhdl
+++ Hardware/ef3-vhdl/src/reset_generator.vhdl
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------
--- 
+--
 -- (c) 2010 Thomas 'skoe' Giesel
 --
 -- This software is provided 'as-is', without any express or implied
@@ -9,7 +9,7 @@
 -- Permission is granted to anyone to use this software for any purpose,
 -- including commercial applications, and to alter it and redistribute it
 -- freely, subject to the following restrictions:
--- 
+--
 -- 1. The origin of this software must not be misrepresented; you must not
 --    claim that you wrote the original software. If you use this software
 --    in a product, an acknowledgment in the product documentation would be
@@ -27,14 +27,14 @@ use ieee.std_logic_unsigned.all;
 
 
 entity reset_generator is
-    port ( 
+    port (
             clk:                in std_logic;
             phi2_cycle_start:   in std_logic;
             start_reset:        in std_logic;
             n_reset_in:         in std_logic;
-            n_reset:            out std_logic;
-            n_generated_reset:  out std_logic;
-            n_sys_reset:        out std_logic
+            n_reset:            out std_logic; -- any reset
+            n_generated_reset:  out std_logic; -- reset from host
+            n_sys_reset:        out std_logic  -- reset by us
     );
 end reset_generator;
 
@@ -46,7 +46,7 @@ architecture reset_generator_arc of reset_generator is
 
     -- as long as this is '1', n_reset_in is ignored
     signal ignore_reset:        std_logic := '0';
-    
+
     signal n_generated_reset_i: std_logic;
     signal n_sys_reset_i:       std_logic;
 begin
@@ -73,9 +73,9 @@ begin
             -- synchronize reset from C64
             n_sys_reset_i <= ignore_reset or n_reset_in;
         end if;
-    end process cycle_counter; 
+    end process cycle_counter;
 
-    
+
     n_sys_reset <= n_sys_reset_i;
     n_reset <= n_generated_reset_i and n_sys_reset_i;
     n_generated_reset <= n_generated_reset_i;
