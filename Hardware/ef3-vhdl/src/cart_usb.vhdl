023d5fcbe776cfbdcfd91260a761a12de771a364
skoe <skoe@localhost>
2012-03-12 21:16:53 +0100

    USB uses new rd/wr signals

--- Hardware/ef3-vhdl/src/cart_usb.vhdl
+++ Hardware/ef3-vhdl/src/cart_usb.vhdl
@@ -27,21 +27,21 @@ use ieee.numeric_std.all;
 
 entity cart_usb is
     port (
-        clk:            in  std_logic;
-        n_reset:        in  std_logic;
-        enable:         in  std_logic;
-        n_io1:          in  std_logic;
-        n_wr:           in  std_logic;
-        bus_ready:      in  std_logic;
-        cycle_start:    in  std_logic;
-        addr:           in  std_logic_vector(15 downto 0);
-        io1_addr_0x:    in  std_logic;
-        n_usb_rxf:      in  std_logic;
-        n_usb_txe:      in  std_logic;
-        usb_read:       out std_logic;
-        usb_write:      out std_logic;
-        data_out:       out std_logic_vector(7 downto 0);
-        data_out_valid: out std_logic
+        clk:                in  std_logic;
+        n_reset:            in  std_logic;
+        enable:             in  std_logic;
+        n_io1:              in  std_logic;
+        rd:                 in  std_logic;
+        wr:                 in  std_logic;
+        cycle_start:        in  std_logic;
+        addr:               in  std_logic_vector(15 downto 0);
+        io1_addr_0x:        in  std_logic;
+        n_usb_rxf:          in  std_logic;
+        n_usb_txe:          in  std_logic;
+        usb_read:           out std_logic;
+        usb_write:          out std_logic;
+        data_out:           out std_logic_vector(7 downto 0);
+        data_out_valid:     out std_logic
     );
 end cart_usb;
 
@@ -67,7 +67,6 @@ begin
     -- TXF  (TX Ready)   If this bit is set, data can be transmitted
     ---------------------------------------------------------------------------
     create_data_out: process(data_out_valid_i, n_usb_rxf, n_usb_txe, addr, n_io1)
-        variable a : integer range 0 to 31;
     begin
         data_out <= (others => '0');
         if data_out_valid_i = '1' then
@@ -82,35 +81,8 @@ begin
                         data_out <= not n_usb_rxf & not n_usb_txe & "000000";
                     when others => null;
                 end case;
-            elsif false then
-                a := to_integer(unsigned(addr(4 downto 0)));
-                case a is
-	                when  0 => data_out <= x"80";
-	                when  1 => data_out <= x"09"; -- start vector => $8009
-	                -- 2, 3 - don't care
-	                when  4 => data_out <= x"c3";
-	                when  5 => data_out <= x"c2";
-	                when  6 => data_out <= x"cd";
-	                when  7 => data_out <= x"38";
-	                when  8 => data_out <= x"30"; -- CBM80
-	                when  9 => data_out <= x"2c";
-	                when 10 => data_out <= x"09";
-	                when 11 => data_out <= x"de"; -- bit $de09
-	                when 12 => data_out <= x"10";
-	                when 13 => data_out <= x"fb"; -- bpl ...
-	                when 14 => data_out <= x"ad";
-	                when 15 => data_out <= x"0a";
-	                when 16 => data_out <= x"de"; -- lda $de0a
-	                when 17 => data_out <= x"48"; -- pha
-	                when 18 => data_out <= x"ca"; -- dex
-	                when 19 => data_out <= x"d0";
-	                when 20 => data_out <= x"f4"; -- bne ...
-	                when 21 => data_out <= x"60"; -- rts
-
-	                when others => null;
-	            end case;
-	        end if;
-	    end if;
+            end if;
+        end if;
     end process;
 
     ---------------------------------------------------------------------------
@@ -124,8 +96,8 @@ begin
             usb_read  <= '0';
             usb_write <= '0';
             if enable = '1' then
-                if io1_addr_0x = '1' and bus_ready = '1' then
-                    if n_wr = '0' then
+                if io1_addr_0x = '1' then
+                    if wr = '1' then
                         case addr(3 downto 0) is
                             when x"a" =>
                                 -- $de0a - write data
@@ -133,7 +105,8 @@ begin
 
                             when others => null;
                         end case;
-                    else
+                    end if;
+                    if rd = '1' then
                         case addr(3 downto 0) is
                             when x"8" =>
                                 -- $de08 - read ID register
@@ -150,7 +123,7 @@ begin
                             when others => null;
                         end case;
                     end if;
-                end if; -- bus_ready...
+                end if; -- io1_addr_0x...
                 if cycle_start = '1' then
                     data_out_valid_i <= '0';
                 end if;
