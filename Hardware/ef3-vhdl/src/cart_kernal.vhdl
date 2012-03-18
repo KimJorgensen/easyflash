cc1573726ae08bfca5a1845ca617625a8e6c0740
skoe <skoe@localhost>
2012-03-18 19:50:45 +0100

    KERNAL uses DMA again

--- Hardware/ef3-vhdl/src/cart_kernal.vhdl
+++ Hardware/ef3-vhdl/src/cart_kernal.vhdl
@@ -40,11 +40,14 @@ entity cart_kernal is
         data:               in  std_logic_vector(7 downto 0);
         button_crt_reset:   in  std_logic;
         flash_addr:         out std_logic_vector(16 downto 0);
+        n_dma:              out std_logic;
         a14:                out std_logic;
         n_game:             out std_logic;
         n_exrom:            out std_logic;
         start_reset:        out std_logic;
         flash_read:         out std_logic;
+        ram_read:           out std_logic;
+        ram_write:          out std_logic;
         hiram:              out std_logic
     );
 end cart_kernal;
@@ -52,6 +55,7 @@ end cart_kernal;
 architecture behav of cart_kernal is
     signal kernal_space_addressed:  boolean;
     signal kernal_space_cpu_read:   boolean;
+    signal kernal_space_cpu_write:  boolean;
     signal kernal_read_active:      boolean;
     signal bank:                    std_logic_vector(2 downto 0);
 begin
@@ -60,6 +64,10 @@ begin
 
     kernal_space_cpu_read <= true when kernal_space_addressed and
         phi2 = '1' and ba = '1' and n_wr = '1'
+        else false;
+
+    kernal_space_cpu_write <= true when kernal_space_addressed and
+        phi2 = '1' and n_wr = '0'
         else false;
 
     start_reset <= enable and button_crt_reset;
@@ -79,42 +87,94 @@ begin
 
     ---------------------------------------------------------------------------
     --
+    -- VIC-II needs address input in this time window (times after Phi2):
+    --      min: Trhl_min - Tasrin = 155 ns - 25 ns = 130 ns
+    --      max: Trhl_max + Trahin = 190 ns + 0 ns  = 190 ns
+    -- phase_pos(3) ends at 160..200 ns, so we can set DMA at this cycle
+    --
+    --
+    -- DMA bei:     0   1   2   3   4   5   6   7   8
+    -- 250407:      OK  OK  OK  OK  NOK ?   ?   NOK NOK
+    -- 250469:      OK  OK  OK  OK  OK  ?   ?   NOK NOK
+    -- Zwei Zyklen:
+    -- 250407:          OK> OK>
+    -- 250469:          OK> OK>
+    -- nok: 7, 8
     ---------------------------------------------------------------------------
-    detect_hiram: process(enable, clk)
+    detect_hiram: process(n_reset, phi2, clk)
     begin
-        if rising_edge(clk) then
+        if n_reset = '0' then
+            n_game  <= '1';
+            n_exrom <= '1';
+            a14 <= '1';
+            n_dma <= '1';
+            hiram <= '0';
+            kernal_read_active <= false;
+
+            flash_read <= '0';
+            ram_read <= '0';
+            ram_write <= '0';
+
+        elsif rising_edge(clk) then
+            flash_read <= '0';
+            ram_read <= '0';
+            ram_write <= '0';
+
             if enable = '1' then
-                if phase_pos(5) = '1' and kernal_space_cpu_read then
-                    -- Address lines are tristated/pulled up now
+
+                if phase_pos(0) = '1' and kernal_space_cpu_read then
+                    n_dma <= '0';
                     n_game  <= '0';
                     n_exrom <= '0';
-                    a14 <= '0';
                     kernal_read_active <= true;
                 end if;
-                if phase_pos(7) = '1' and kernal_read_active then
-                    -- ROMH reflects HIRAM now
-                    a14 <= '1';
-                    if n_romh = '1' then
-                        -- ram
-                        n_game  <= '1';
-                        n_exrom <= '1';
-                    else
-                        -- rom
+
+                if kernal_read_active then
+                    if phase_pos(1) = '1' then
+                        a14 <= '0';
+                        n_dma <= '1';
+                    end if;
+
+                    if phase_pos(3) = '1' then
+                        -- Release everything and check for hiram
+                        a14 <= '1';
+                        n_dma <= '1';
+                        -- ROMH reflects HIRAM now
+                        if n_romh = '1' then
+                            -- ram
+                            ram_read <= '1';
+                        else
+                            -- rom
+                            flash_read <= '1';
+                        end if;
                         n_exrom <= '1'; -- Ultimax mode
                     end if;
+                end if; -- kernal_read_active
+
+                if phase_pos(4) = '1' and kernal_space_cpu_write then
+                    ram_write <= '1';
                 end if;
+
                 if cycle_start = '1' then
                     -- KERNAL read complete
                     n_game  <= '1';
                     n_exrom <= '1';
                     a14 <= '1';
+                    --n_dma <= '1';
                     kernal_read_active <= false;
                 end if;
-            else
+            else -- enable
                 n_game  <= '1';
                 n_exrom <= '1';
                 a14 <= '1';
+                n_dma <= '1';
                 hiram <= '0';
+                kernal_read_active <= false;
+
+                flash_read <= '0';
+                ram_read <= '0';
+                ram_write <= '0';
+
             end if; -- enable
         end if; -- clk
     end process;
@@ -128,24 +188,6 @@ begin
     begin
         flash_addr <= (others => '0');
         flash_addr(15 downto 0) <= bank & addr(12 downto 0);
-    end process;
-
-    ---------------------------------------------------------------------------
-    -- Combinatorical logic to prepare the address to be latched to the
-    -- internal address bus. Additionally the signal which enables
-    -- the latches at the next CLK cycle is prepared here.
-    ---------------------------------------------------------------------------
-    create_mem_ctrl: process(enable, addr, phase_pos,
-                             kernal_space_cpu_read)
-    begin
-        flash_read <= '0';
-
-        if enable = '1' then
-            if phase_pos(3) = '1' and kernal_space_cpu_read then
-                -- start speculative flash read to hide its latency
-                flash_read <= '1';
-            end if;
-        end if;
     end process;
 
 end architecture behav;
