d0db22c0c48c84224ff32bf868e914d8c361fe7f
uffe <uffe@localhost>
2012-11-04 22:59:56 +0100

    Version Screen implemented

--- EF3BootImage/efmenu/src/memcfg.h
+++ EF3BootImage/efmenu/src/memcfg.h
@@ -3,9 +3,9 @@
 #define MEMCFG_H
 
 #define P_VIC_BASE      ((uint8_t*)0x4000)
-#define P_GFX_BITMAP    ((uint8_t*)0x4000)
-#define P_GFX_COLOR     ((uint8_t*)0x6000)
-#define P_WND_SPRITES   ((uint8_t*)0x6800)
+#define P_GFX_COLOR     ((uint8_t*)0x5C00)
+#define P_GFX_BITMAP    ((uint8_t*)0x6000)
+//#define P_WND_SPRITES   ((uint8_t*)0x6800)
 
 #define P_SPR_PTRS      (GFX_COLOR + 0x3f8)
 
