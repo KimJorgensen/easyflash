2bc5639445b7ea847531c82a797f76bd19ba8b36
skoe <skoe@localhost>
2013-01-21 22:12:31 +0100

    Write code improved, checksum code added

--- libs/eload/src/drive_1541_write.s
+++ libs/eload/src/drive_1541_write.s
@@ -32,6 +32,8 @@
 .import drv_1541_recv
 .import drv_1541_send
 .import drv_1541_search_header
+.import drv_1541_wait_sync
+.import drv_1541_move_head
 
 .export drive_code_1541_write
 drive_code_1541_write  = *
@@ -50,23 +52,26 @@ loop:
         ldy #4                  ; receive job to buffer (does SEI when rx)
         jsr drv_1541_recv_to_buffer
 
-        ldx buffer              ; job
-        bne write_sector
-ret:
-        rts                     ; leave overlay code
-
-; =============================================================================
-;
-; Write a GCR sector to disk. Returns clc if successful, sec if error
-;
-; =============================================================================
-write_sector:
         lda buffer + 1
         sta job_track_backup
         lda buffer + 2
         sta job_sector_backup
 
-        ; receive 69 + 256 bytes of GCR encodec track
+        ldx buffer              ; job
+        dex
+        beq write_sector
+        dex
+        beq track_checksum
+ret:
+        rts                     ; leave overlay code
+
+; =============================================================================
+;
+; Write a GCR sector to disk.
+;
+; =============================================================================
+write_sector:
+        ; receive 69 + 256 bytes of GCR encoded track
         ldy #gcr_overflow_size
         lda #<gcr_overflow_buff
         ldx #>gcr_overflow_buff
@@ -77,54 +82,48 @@ write_sector:
         jsr drv_1541_restore_orig_job
         jsr drv_1541_prepare_read
         jsr drv_1541_search_header
+        bcs status_motor_off_ret ; error: code in A already
 
-        bcs motor_off_and_ret   ; error: code in A already
-        ldx #8                  ; skip 9 bytes after header
+        ldx #8                  ; skip last byte of header + 8 bytes gap
 @skip9:
-        bvc @skip9              ; wait for byte ready
-        clv                     ; clear byte ready (V)
+        wait_byte_ready
         dex
         bpl @skip9
+                                ; the 9th byte gap is being read
+                                ; while we change to write mode
         stx $1c03               ; data port output
-        lda $1c0c
-        and #$df                ; %111x => %110x
+        stx $1c01               ; write $ff = sync
+        lda #$ce
         sta $1c0c               ; write mode
 
-        ; write_sync
-        lda #$ff
-        ldy #5
-        sta $1c01               ; write $ff = sync
-:
-        bvc :-                  ; wait for byte ready
-        clv                     ; clear byte ready (V)
+        ldy #5                  ; write 5 bytes sync
+@wrsync:
+        wait_byte_ready
         dey
-        bne :-
+        bne @wrsync
 
         ldy #$bb
 @write_data_1:
         lda $0100, y
-        wait_byte_ready
         sta $1c01               ; write data byte
+        wait_byte_ready
         iny
         bne @write_data_1
 
 @write_data_2:
         lda buffer, y
-        wait_byte_ready
         sta $1c01               ; write data byte
+        wait_byte_ready
         iny
         bne @write_data_2
-        wait_byte_ready
-        jsr $fe00               ; head to read mode
-        clc                     ; mark for success
-motor_off_and_ret:
-        pha
-        jsr $f98f               ; prepare motor off (doesn't change C)
-        lda $1c00
-        and #$f7                ; LED off
-        sta $1c00
-        pla
 
+        bvc *                   ; wait last byte ready
+        lda #$ee                ; read mode
+        sta $1c0c
+        sty $1c03               ; port A (read/write head) to input
+
+        clc                     ; mark for success
+status_motor_off_ret:
         bcs :+                  ; C set == error
         lda #ELOAD_OK
 :
@@ -134,7 +133,129 @@ motor_off_and_ret:
         jsr drv_1541_send
         lda status + 1
         jsr drv_1541_send
+motor_off_and_ret:
+        jsr $f98f               ; prepare motor off (doesn't change C)
+        lda $1c00
+        and #$f7                ; LED off
+        sta $1c00
         jmp loop
+
+; =============================================================================
+;
+;
+; =============================================================================
+track_checksum:
+        jsr drv_1541_restore_orig_job
+        jsr drv_1541_prepare_read
+        lda job_track
+        cmp #1
+        bne @move_head_only     ; for all tracks except #1
+        jsr drv_1541_search_header
+@move_head_only:
+        jsr drv_1541_move_head
+        lda $43                 ; sectors per track
+        sta job_sector          ; counter
+
+        ldy #0
+        sty buff_ptr            ; used as index to buffer $0700
+@next_sector:
+        ldx #2                  ; retry counter, may find a data block first
+@retry_header:
+        jsr drv_1541_wait_sync
+        bcs error
+@check_header:
+        wait_byte_ready
+        lda $1c01               ; read data from head
+        cmp #$52                ; Is it a header?
+        beq @header_found
+        dex
+        bne @retry_header
+        beq @no_header
+@header_found:
+        ldy buff_ptr
+        sta buffer, y
+        iny
+        ldx #9                  ; 9 GCR bytes left
+@copy_header:
+        wait_byte_ready
+        lda $1c01               ; read data from head
+        sta buffer, y
+        iny
+        dex
+        bne @copy_header
+        sty buff_ptr
+
+        ; === Wait for data block and calculate its checksum ===
+        jsr checksum16
+
+        dec job_sector
+        bne @next_sector
+
+        ; fertig!
+        ; send status
+        lda #ELOAD_OK
+        jsr drv_1541_send
+        lda job_track
+        jsr drv_1541_send
+        lda job_sector
+        jsr drv_1541_send
+        ; send data block, always 256 bytes
+        ldx #0
+@send_buffer:
+        lda buffer, x
+        jsr drv_1541_send
+        inx
+        bne @send_buffer
+
+        jmp motor_off_and_ret
+
+@no_header:
+        lda #ELOAD_HEADER_NOT_FOUND
+        sec
+error:
+        jmp status_motor_off_ret
+
+; Depending from the bit rate we have 26 to 32 cycles per byte,
+; so our loop must take less then 26 cycles.
+checksum16:
+        ldx #$bb        ; init counter
+        lda #0
+        sta zptmp       ; high byte = 0
+
+        jsr drv_1541_wait_sync
+        bcs error
+
+        ; y is 0 after drv_1541_wait_sync => init high byte
+        ; c is clear on success, addition prepared
+
+        tya             ; low byte = 0
+:
+        bvc :-          ;   2    2
+        eor $1c01       ;   4    6      sum += value + carry; new carry
+        clv             ;   2    8
+        rol zptmp       ;   5   13      carry << high << carry
+        rol             ;   2   15      carry << low << carry
+        inx             ;   2   17
+        bne :-          ;   3   20
+
+:
+        bvc :-          ;   2    2
+        eor $1c01       ;   4    6      sum += value + carry; new carry
+        clv             ;   2    8
+        rol zptmp       ;   5   13      carry << high << carry
+        rol             ;   2   15      carry << low << carry
+        inx             ;   2   17
+        bne :-          ;   3   20
+
+.assert >checksum16 = >*, error, "page boundary crossed"
+        ldx buff_ptr
+        sta buffer, x
+        inx
+        lda zptmp       ; high
+        sta buffer, x
+        inx
+        stx buff_ptr
+        rts
 
 
 drive_code_1541_write_size  = * - drive_code_1541_write_start
